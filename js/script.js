96d3aea43ceae611d6f7f3da57518cf88bc2ed76
Akash Saxena <akashsaxena5555@gmail.com>
2022-05-03 18:36:31 +0530

    integrate status button in object logic

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ function Book(title,author,numberOfPages,alreadyRead)
     this.title = title
     this.author = author
     this.pages = numberOfPages
-    this.read = alreadyRead
+    this.read = `${this.read ? 'Yes' : 'No'}` 
 
     this.info = function(){
-        return title + ' by ' + author + ' is ' + this.pages + ' pages long, whose read status is ' + this.read 
+        return title + ' by ' + author + ' is ' + this.pages + ' pages long, whose read status is '
+         + this.read
     }
 }
 
@@ -20,7 +21,7 @@ function addBookToLibrary()
     let bookEntry = document.getElementById('name');
     let authorEntry = document.getElementById('author');
     let pageCountEntry = document.getElementById('pages');
-    // let readStatusEntry = document.getElementById('readStatus');
+    let readStatus = document.getElementById('readStatus')
 
     if (bookEntry.value == '' || authorEntry.value == '' || pageCountEntry.value == '')
     {
@@ -28,12 +29,13 @@ function addBookToLibrary()
     }
     else
     {
-        let book1 = new Book(bookEntry.value,authorEntry.value,pageCountEntry.value,true)
+        let book1 = new Book(bookEntry.value,authorEntry.value,pageCountEntry.value,readStatus.checked)
         myLibrary.push(book1)
         showBookCard();
         bookEntry.value = ''
         authorEntry.value = ''
         pageCountEntry.value = ''
+        readStatus.checked = false
     }
 
 }
@@ -44,18 +46,20 @@ function showBookCard()
     let bookEntryDiv = document.createElement('div')
     let authorEntryDiv = document.createElement('div')
     let pageCountEntryDiv = document.createElement('div')
-    // let authorEntryDiv = document.createElement('div')
+    let readStatusDiv = document.createElement('div')
 
     bookCard.className = 'card'
 
     bookEntryDiv.textContent += myLibrary[counter].title
     authorEntryDiv.textContent += myLibrary[counter].author
     pageCountEntryDiv.textContent += myLibrary[counter].pages
+    readStatusDiv.textContent += myLibrary[counter].read
     counter++
 
     bookCard.appendChild(bookEntryDiv)
     bookCard.appendChild(authorEntryDiv)
     bookCard.appendChild(pageCountEntryDiv)
+    bookCard.appendChild(readStatusDiv)
 
     gridArea.appendChild(bookCard)
 }
